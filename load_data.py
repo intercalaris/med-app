6301237871a041bacabfabd04027df5d0f584b01
intercalaris <ymaviv@gmail.com>
2023-01-14 19:56:43 -0600

    Small updates, need to update API

--- load_data.py
+++ load_data.py
@@ -1,7 +1,8 @@
-import requests
-import json
 import pandas as pd
+import json
+import requests
 
+# function to fetch data from API
 def fetch_data(query):
     url = "https://uts-ws.nlm.nih.gov/rest/search"
     params = {
@@ -12,7 +13,25 @@ def fetch_data(query):
     data = json.loads(response.text)
     return data
 
-def load_data(query):
-    data = fetch_data(query)
-    data_frame = pd.json_normalize(data['result'])
-    return data_frame
+# function to preprocess data
+def preprocess_data(data):
+    print(data)
+    df = pd.json_normalize(data['result']['results'])
+
+    # convert json to dataframe
+    df = pd.json_normalize(data['result']['results'])
+    # extract relevant columns
+    df = df[['ui', 'name', 'rootSource', 'semanticType', 'description']]
+    # rename columns
+    df = df.rename(columns={'ui': 'code', 'name': 'diagnosis', 'rootSource': 'source', 'semanticType': 'type', 'description': 'text'})
+    # preprocess text column
+    df['text'] = df['text'].str.lower()
+    df['text'] = df['text'].str.replace('[^\w\s]','')
+    # return preprocessed data
+    return df
+
+# fetch data from API
+data = fetch_data("headache")
+
+# preprocess data
+df = preprocess_data(data)
